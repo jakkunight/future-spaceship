75e036d1375f59afbbfa7c294fe595ed95f7665c
Jakku Night <J97k107u45k117n@gmail.com>
2024-06-17 09:25:38 -0400

    Add neon-text effect.

--- src/components/std/Button.tsx
+++ src/components/std/Button.tsx
@@ -5,7 +5,7 @@ export type ButtonProps = HTMLAttributes<HTMLButtonElement> & {
 }
 
 export default function Button({ children, ...props }: ButtonProps) {
-  return <button className={" hover:text-cyan-500 hover:shadow-cyan-300 hover:shadow-[inset_0px_0px_1rem] flex items-center justify-center text-cyan-100 font-bigblueterm border-2 border-cyan-500 h-fit w-fit text-lg p-2"} {...props}>
+  return <button className={" hover:neon-text-zinc-50 flex items-center justify-center text-cyan-100 font-bigblueterm border-2 border-cyan-500 h-fit w-fit text-lg p-2"} {...props}>
     {children}
   </button>;
 }
