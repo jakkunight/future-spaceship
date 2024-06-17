dc773cee166e6abb5a32be03f2718e2c83226a2b
Jakku Night <J97k107u45k117n@gmail.com>
2024-06-17 09:24:51 -0400

    Add backlight effect.

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -2,16 +2,20 @@ import { HTMLAttributes, ReactNode } from "react";
 
 export type DisplayParams = HTMLAttributes<HTMLDivElement> & {
   children?: ReactNode;
+  crtEffect?: boolean;
 };
 
-export default function Display({ children, ...props }: DisplayParams) {
+export default function Display({ children, crtEffect, ...props }: DisplayParams) {
   return <div
-    className="w-full min-h-screen h-full text-white text-4xl font-bigblueterm shadow-[inset_0px_0px_2rem] shadow-cyan-500 bg-[100% 80%] bg-repeat-y bg-gradient-to-b from-transparent via-cyan-900/50 via-50% animate-oldcrt"
-    //className="w-full min-h-screen h-full text-white text-4xl font-bigblueterm shadow-[inset_0px_0px_2rem] shadow-cyan-500 bg-[100% 80%] bg-repeat-y"
+    className={
+      `scanlines-gray-500 
+      flex flex-col items-center justify-center 
+      w-full min-h-screen h-full 
+      text-white text-4xl font-bigblueterm 
+      shadow-[inset_0px_0px_2rem] shadow-cyan-500 
+      ${crtEffect ? "bg-[100% 80%] bg-repeat-y bg-gradient-to-b from-transparent via-cyan-900/50 via-50% animate-oldcrt" : ""}`
+    }
     {...props} >
-    <div
-      className="scanlines-gray-500 w-full min-h-screen h-full flex flex-col items-center justify-center" {...props}>
-      {children}
-    </div>
+    {children}
   </div>;
 }
