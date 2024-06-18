fc39b18c8d23fe6f444d10fac69cd071711ee9c1
Jakku Night <J97k107u45k117n@gmail.com>
2024-06-18 10:31:13 -0400

    Clean component.

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -8,12 +8,7 @@ export type DisplayParams = HTMLAttributes<HTMLDivElement> & {
 export default function Display({ children, crtEffect, ...props }: DisplayParams) {
   return <div
     className={
-      `scanlines-gray-500 
-      flex flex-col items-center justify-center 
-      w-full min-h-screen h-full 
-      text-white text-4xl font-bigblueterm 
-      shadow-[inset_0px_0px_2rem] shadow-cyan-500 
-      ${crtEffect ? "bg-[100% 80%] bg-repeat-y bg-gradient-to-b from-transparent via-cyan-900/50 via-50% animate-oldcrt" : ""}`
+      `scanlines-cyan-950 border-2 border-cyan-500 flex flex-col items-center justify-center w-full min-h-screen h-full text-white text-4xl font-bigblueterm shadow-[inset_0px_0px_2rem] shadow-cyan-500 ${crtEffect ? "bg-[100% 80%] bg-repeat-y bg-gradient-to-b from-transparent via-cyan-900/50 via-50% animate-oldcrt" : ""}`
     }
     {...props} >
     {children}
